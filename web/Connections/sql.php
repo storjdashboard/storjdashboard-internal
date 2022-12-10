79abda112dfaf715dcd6cb24b9b2b4d3d77f6e5e
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2022-12-10 17:12:27 +0000

    Add files via upload

---
+++ web/Connections/sql.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors', 1);
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_sql = "localhost";
+$database_sql = "storj_dashboard";
+$username_sql = "";
+$password_sql = "";
+$sql = mysqli_connect($hostname_sql, $username_sql, $password_sql); 
+
+if (!isset($_SESSION)) {
+  session_start();
+}
+?>
