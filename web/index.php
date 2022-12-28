07f207170919b3e7b8b317d5013a724fab7ddf1e
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2022-12-28 13:55:05 +0000

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -11,7 +11,8 @@ $start = $time;
 
 <?php if(!isset($_GET['page']) && file_exists("install")){header("location: install");exit;}?>
 <?php if(isset($_GET['page']) && $_GET['page']=='install' && isset($_GET['complete']) && $_GET['complete']==1) {
-	if(@rmdir("$root_dir/install")){header("location ./");}else{ $output = null; $code = null;$command = "cd $root_dir && rmdir -r install";exec($command,$output,$code);}if(file_exists('install')){ echo "<h2>Warning: Unable to remove install directory<br><br>You must remove INSTALL folder manually if there is no FAIL tasks.<br>Otherwise you will need to fix the FAIL tasks manually.</h2>"; } exit; }?>
+	if(@rmdir("install")){header("location ./");}
+	if(file_exists('install')){ echo "<h2>Warning: Unable to remove install directory<br><br>You must remove INSTALL folder manually if there is no FAIL tasks.<br>Otherwise you will need to fix the FAIL tasks manually.</h2>"; } exit; }?>
 <?php require_once("cfg.php"); ?>
 <?php if($config_row['restrict']==1){ require_once($resitrct_file); } ?>
 <?php require_once($sql_conn_file); ?>
