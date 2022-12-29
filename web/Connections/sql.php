a66614293a3a8dc670443089f8fffee876267a76
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2022-12-29 11:23:19 +0000

    Update sql.php

--- web/Connections/sql.php
+++ web/Connections/sql.php
@@ -1,8 +1,6 @@
 <?php
 ini_set('display_errors', 0);
-# FileName="Connection_php_mysql.htm"
-# Type="MYSQL"
-# HTTP="true"
+
 $hostname_sql = "localhost";
 $database_sql = "storj_dashboard";
 $username_sql = "";
