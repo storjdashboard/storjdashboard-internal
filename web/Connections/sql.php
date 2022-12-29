ac3c85ba2f7f9debf8424874e3e4bdcfe61e6a32
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2022-12-29 11:23:05 +0000

    Update sql.php

--- web/Connections/sql.php
+++ web/Connections/sql.php
@@ -7,7 +7,7 @@ $hostname_sql = "localhost";
 $database_sql = "storj_dashboard";
 $username_sql = "";
 $password_sql = "";
-$sql = mysqli_connect($hostname_sql, $username_sql, $password_sql); 
+$sql = mysqli_connect($hostname_sql, $username_sql, $password_sql, $database_sql); 
 
 if (!isset($_SESSION)) {
   session_start();
