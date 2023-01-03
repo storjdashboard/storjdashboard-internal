0bb4292f44670d1eee9b13d9cee16a70865bcf46
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2023-01-03 11:17:05 +0000

    Update fileDownload.php
    updated 'v' , not required in the filename for downloading

--- web/include_content/scripts/update/fileDownload.php
+++ web/include_content/scripts/update/fileDownload.php
@@ -5,8 +5,8 @@
 	$directory = $root_dir."/include_content/scripts/update/downloads/";
 	//https://github.com/storjdashboard/storjdashboard-internal/archive/refs/tags/v1.0.0.1.tar.gz
     //$url = "https://github.com/storjdashboard/storjdashboard-internal/archive/refs/tags/v$new_version.tar.gz"; DEBUG
-$url = "https://github.com/storjdashboard/storjdashboard-internal/archive/refs/tags/v$new_version.tar.gz";
-      echo "attempting: https://github.com/storjdashboard/storjdashboard-internal/archive/refs/tags/v$new_version.tar.gz<br>";
+$url = "https://github.com/storjdashboard/storjdashboard-internal/archive/refs/tags/$new_version.tar.gz";
+      echo "attempting: https://github.com/storjdashboard/storjdashboard-internal/archive/refs/tags/$new_version.tar.gz<br>";
     // Use basename() function to return the base name of file
     $file_name = basename($url);
       
