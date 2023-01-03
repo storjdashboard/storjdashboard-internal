cbf85e28bd310b2299263bb9fd83f384d206b4b7
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2023-01-03 09:35:00 +0000

    Update create_db.sql
    removed fixed collation type.
    added insert for required default rows.

--- web/install/sql/create_db.sql
+++ web/install/sql/create_db.sql
@@ -1,33 +1,18 @@
 -- --------------------------------------------------------
--- Host:                         192.168.0.108
--- Server version:               8.0.31-0ubuntu0.20.04.2 - (Ubuntu)
 -- Server OS:                    Linux
--- HeidiSQL Version:             12.3.0.6589
+-- Project:			 StorjDashboard-Internal
 -- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- Dumping structure for table storj_dashboard.config
 CREATE TABLE `config` (
 	`id` INT(10) NOT NULL,
-	`version` VARCHAR(10) NOT NULL DEFAULT '1' COLLATE 'utf8mb4_0900_ai_ci',
+	`version` VARCHAR(10) NOT NULL DEFAULT '1',
 	`show_live_bw` INT(10) NOT NULL DEFAULT '0',
 	`show_server_info` INT(10) NOT NULL DEFAULT '0',
 	`restrict` INT(10) NOT NULL DEFAULT '0',
 	PRIMARY KEY (`id`) USING BTREE
-)
-COLLATE='utf8mb4_0900_ai_ci'
-ENGINE=InnoDB
-;
+);
 
--- Data exporting was unselected.
 
 -- Dumping structure for table storj_dashboard.docker
 CREATE TABLE IF NOT EXISTS `docker` (
@@ -35,12 +20,11 @@ CREATE TABLE IF NOT EXISTS `docker` (
   `docker_name` tinytext NOT NULL,
   `server_ip` tinytext NOT NULL,
   `port` int NOT NULL DEFAULT '4243',
-  `user` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `pw` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `user` tinytext,
+  `pw` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='adding/removing docker';
+)COMMENT='adding/removing docker';
 
--- Data exporting was unselected.
 
 -- Dumping structure for table storj_dashboard.login
 CREATE TABLE IF NOT EXISTS `login` (
@@ -49,9 +33,7 @@ CREATE TABLE IF NOT EXISTS `login` (
   `pw` mediumtext NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_UNIQUE` (`user`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Data exporting was unselected.
+);
 
 -- Dumping structure for table storj_dashboard.nodes
 CREATE TABLE IF NOT EXISTS `nodes` (
@@ -59,9 +41,7 @@ CREATE TABLE IF NOT EXISTS `nodes` (
   `ip` varchar(45) NOT NULL,
   `port` varchar(45) NOT NULL,
   PRIMARY KEY (`node_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Data exporting was unselected.
+) ;
 
 -- Dumping structure for table storj_dashboard.paystubs
 CREATE TABLE IF NOT EXISTS `paystubs` (
@@ -75,12 +55,7 @@ CREATE TABLE IF NOT EXISTS `paystubs` (
   `paid` int NOT NULL DEFAULT '0',
   `distributed` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='api/heldamount/paystubs/2000-01/YYYY-MM';
+) COMMENT='api/heldamount/paystubs/2000-01/YYYY-MM';
 
--- Data exporting was unselected.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+INSERT INTO `config` (`id`, `show_live_bw`, `show_server_info`, `restrict`) VALUES (0, 0, 0, 1);
+INSERT INTO `login` (`user`, `pw`) VALUES ('admin', 'password');
