d04bd8f3678f41fd97ec0d018e45b888bab380e9
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2023-01-08 16:51:38 +0000

    added try/catch for catching sql failed connection

--- web/Connections/sql.php
+++ web/Connections/sql.php
@@ -1,11 +1,16 @@
 <?php
-ini_set('display_errors', 0);
+ini_set('display_errors', 0 ); //change value to 1 instead of 0 for debugging
 
 $hostname_sql = "localhost";
-$database_sql = "storj_dashboard";
+$database_sql = "";
 $username_sql = "";
 $password_sql = "";
-$sql = mysqli_connect($hostname_sql, $username_sql, $password_sql, $database_sql); 
+$sql = "";
+
+if($hostname_sql=="" || $database_sql==""  || $username_sql==""  || $password_sql==""){
+	echo "<br>SQL File is not complete. (/Connections/sql.php)<br>"; }else{
+try{ $sql = mysqli_connect($hostname_sql, $username_sql, $password_sql, $database_sql); } catch (mysqli_sql_exception $e) { $sql_error = $e->getMessage(); }
+}
 
 if (!isset($_SESSION)) {
   session_start();
