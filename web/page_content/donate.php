67549b66bf5daef6b5d4b20bb7c9cb1198cf2bff
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2023-01-23 22:19:22 +0000

    Add files via upload

---
+++ web/page_content/donate.php
@@ -0,0 +1,74 @@
+<div class="container-fluid">
+
+                    <!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Donate</h1>
+                        <a href="./" class="d-none d-sm-inline-block btn btn-sm btn-danger shadow-sm"><i class="fas fa fa-long-arrow-left"></i> Back</a>
+                    </div>
+
+                    <!-- Content Row --><!-- Content Row --><!--- ROW --><!-- Content Row -->
+
+                    <div class="row">
+
+                        <!-- Area Chart -->
+                        <div class="col-xl-12 col-lg-12">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-dark">Donate</h6>
+                                    
+                                </div>
+                                <!-- Card Body -->
+                                <div class="card-body">
+        <div class="row">
+          <div class="col-12 ">
+                        <div class="p-1">
+			                                <p>Over the past 2 years, I and an ex-colleague have been running our own Storj machines however we have always felt we lacked more information about Storj and how it was performing outside of our own nodes.</p>
+                              <p>So we decided to dedicate some time to get our own dashboards working and then implemented these onto a cloud platform that we would be able to offer to multiple people.</p>
+                              <p>We are now in a very comfortable place where we can see what is happening with the nodes on a daily/monthly basis.</p>
+                              <p>We do still have much more work we want to put into the StorjDashboard project.</p>
+                              <p>Of course, we do this for free and for the community as a whole.</p>
+                              <p>However, if you feel you have benefited from using our project and would like to donate something towards the ongoing maintenance and development of the dashboard you can use images below to scan or you can send a manual payment to us.</p>
+                              <p>Cheers</p>
+                              <p>Hass &amp; Brando</p>
+			  </div>
+                    </div>
+                </div>
+                                </div>
+                            </div>
+                        </div>
+						
+<div class="col-xl-12 col-lg-12">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-dark">Ways to Donate</h6>
+                                    
+                                </div>
+                                <!-- Card Body -->
+                                <div class="card-body">
+        <div class="row">
+          <div class="col-12 ">
+                        <div class="p-1">
+                          <p><strong>zkSync Address:</strong> 0xFc3c8bCb93cF12Ca91c250565220C995D5671e67 | <a href="https://checkout.zksync.io/link/MHhGYzNjOGJDYjkzY0YxMkNhOTFjMjUwNTY1MjIwQzk5NUQ1NjcxZTY3fFNUT1JKfDUw" target="_blank"><strong>Send 50 STORJ</strong></a><strong></strong> | <a href="https://checkout.zksync.io/link/MHhGYzNjOGJDYjkzY0YxMkNhOTFjMjUwNTY1MjIwQzk5NUQ1NjcxZTY3fFNUT1JKfDIw" target="_blank"><strong>Send 20 STORJ</strong></a></p>
+                          <p><strong>ETH  Address:</strong> 0xFc3c8bCb93cF12Ca91c250565220C995D5671e67</p>
+                          <p>&nbsp;</p>
+                          <table width="100%" border="0" cellspacing="5" cellpadding="0">
+                            <tbody>
+                              <tr>
+                                <th align="center" valign="top" nowrap="nowrap" scope="col">Binance Pay<br>                                  <img class="img-responsive" style="max-height: 600px;" src="https://storjdashboard.com/images/donate/binance_pay_donate.jpg" alt="Binance Pay"/></th>
+                                <th align="center" valign="top" nowrap="nowrap" scope="col">ETH Address <br>                                  
+                                <img class="img-responsive" style="max-height: 600px;" src="https://storjdashboard.com/images/donate/ETH-address.png" alt="Binance Pay"/></th>
+                                <th align="center" valign="top" nowrap="nowrap" scope="col">&nbsp;</th>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+          </div>
+                </div>
+                                </div>
+                            </div>
+                        </div>						
+
+<!-- Content Row --></div>
+                    
